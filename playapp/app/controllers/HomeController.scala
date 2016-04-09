c5092c6211fbce3f5b7db096b346066d54995cd7
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2016-04-09 11:28:10 +0900

    Renamed controller class of sample play app

--- playapp/app/controllers/HomeController.scala
+++ playapp/app/controllers/HomeController.scala
@@ -7,9 +7,9 @@ import play.api.Logger
 import play.api.mvc._
 
 @Singleton
-class Application @Inject() (flyways: Flyways) extends Controller {
+class HomeController @Inject() (flyways: Flyways) extends Controller {
 
-  private val logger = Logger(classOf[Application])
+  private val logger = Logger(classOf[HomeController])
 
   def index = Action {
     Ok(views.html.index(flyways, "Your new application is ready."))
